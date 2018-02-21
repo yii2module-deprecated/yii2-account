615e221d2168677c472772d723d4ac4c35868f25
Vitaliy <theyamshikov@yandex.ru>
2018-02-21 17:00:52 +0600

    move tps drivers

--- src/domain/v1/repositories/core/RegistrationRepository.php
+++ src/domain/v1/repositories/core/RegistrationRepository.php
@@ -3,7 +3,7 @@
 namespace yii2module\account\domain\v1\repositories\core;
 
 use yii2module\account\domain\v1\interfaces\repositories\RegistrationInterface;
-use yii2module\account\domain\v1\repositories\tps\RegistrationRepository as TpsRegistrationRepository;
+use yii2woop\common\domain\account\v1\repositories\tps\RegistrationRepository as TpsRegistrationRepository;
 
 class RegistrationRepository extends TpsRegistrationRepository implements RegistrationInterface {
 
