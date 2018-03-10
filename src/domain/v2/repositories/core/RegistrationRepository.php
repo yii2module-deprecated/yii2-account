bfb3de886c04624f6e896a3dc1a782fd8ab757db
Vitaliy <theyamshikov@yandex.ru>
2018-03-11 00:44:12 +0600

    fix

--- src/domain/v2/repositories/core/RegistrationRepository.php
+++ src/domain/v2/repositories/core/RegistrationRepository.php
@@ -2,9 +2,23 @@
 
 namespace yii2module\account\domain\v2\repositories\core;
 
+use Yii;
+use yii2lab\domain\repositories\BaseRepository;
+use yii2module\account\domain\v2\helpers\ConfirmHelper;
 use yii2module\account\domain\v2\interfaces\repositories\RegistrationInterface;
-use yii2woop\common\domain\account\v1\repositories\tps\RegistrationRepository as TpsRegistrationRepository;
 
-class RegistrationRepository extends TpsRegistrationRepository implements RegistrationInterface {
-
+class RegistrationRepository extends BaseRepository implements RegistrationInterface {
+	
+	public function generateActivationCode() {
+		return ConfirmHelper::generateCode();
+	}
+	
+	public function create($data) {
+		Yii::$app->account->login->create($data);
+	}
+	
+	public function isExists($login) {
+		return $this->domain->repositories->login->isExistsByLogin($login);
+	}
+	
 }
