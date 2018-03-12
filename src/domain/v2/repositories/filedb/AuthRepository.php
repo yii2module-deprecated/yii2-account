1184422d6d33b3f562c76408e378cd2246e3df83
Vitaliy <theyamshikov@yandex.ru>
2018-03-13 00:46:05 +0600

    fix

--- src/domain/v2/repositories/filedb/AuthRepository.php
+++ src/domain/v2/repositories/filedb/AuthRepository.php
@@ -3,24 +3,7 @@
 namespace yii2module\account\domain\v2\repositories\filedb;
 
 use yii2module\account\domain\v2\interfaces\repositories\AuthInterface;
-use yii2lab\domain\repositories\BaseRepository;
-use Yii;
 
-class AuthRepository extends BaseRepository implements AuthInterface {
-
-	public function authentication($login, $password) {
-		$user = $this->domain->getRepositories()->login->oneByLogin($login);
-		if(empty($user)) {
-			return false;
-		}
-		if(!Yii::$app->security->validatePassword($password, $user->password_hash)) {
-			return false;
-		}
-		return $user;
-	}
-	
-	public function setToken($token) {
-	
-	}
+class AuthRepository extends \yii2module\account\domain\v2\repositories\ar\AuthRepository implements AuthInterface {
 
 }
