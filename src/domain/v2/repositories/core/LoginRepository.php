df28217b3fe4f6894e67a86935a7693bbe7f764c
Vitaliy <theyamshikov@yandex.ru>
2018-03-27 21:39:04 +0600

    replace core parent

--- src/domain/v2/repositories/core/LoginRepository.php
+++ src/domain/v2/repositories/core/LoginRepository.php
@@ -3,16 +3,15 @@
 namespace yii2module\account\domain\v2\repositories\core;
 
 use yii\web\NotFoundHttpException;
-use yii\web\UnauthorizedHttpException;
-use yii2lab\domain\repositories\ActiveCoreRepository;
+use yii2lab\core\domain\repositories\base\BaseActiveCoreRepository;
 use yii2lab\misc\enums\HttpHeaderEnum;
 use yii2lab\rest\domain\helpers\RestHelper;
 use yii2module\account\domain\v2\interfaces\repositories\LoginInterface;
 
-class LoginRepository extends ActiveCoreRepository implements LoginInterface {
+class LoginRepository extends BaseActiveCoreRepository implements LoginInterface {
 	
-	public $baseUri = 'user';
-	public $version = 'v1';
+	public $point = 'user';
+	public $version = 1;
 	
 	public function isExistsByLogin($login) {
 		try {
@@ -30,7 +29,7 @@ class LoginRepository extends ActiveCoreRepository implements LoginInterface {
 	
 	public function oneByToken($token, $type = null) {
 		//$response = $this->get('\auth', [], [HttpHeaderEnum::AUTHORIZATION => $token]);
-		$url = env('servers.core.domain') . $this->version . SL . 'auth';
+		$url = env('servers.core.domain') . 'v' . $this->version . SL . 'auth';
 		$response = RestHelper::get($url, [], [HttpHeaderEnum::AUTHORIZATION => $token]);
 		return $this->forgeEntity($response->data);
 	}
