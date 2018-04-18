2bdfaaf99e582c09bb38205752f87043cb422aed
Vitaliy <theyamshikov@yandex.ru>
2018-04-19 00:47:18 +0600

    clean

--- src/domain/v2/repositories/core/SecurityRepository.php
+++ src/domain/v2/repositories/core/SecurityRepository.php
@@ -2,10 +2,7 @@
 
 namespace yii2module\account\domain\v2\repositories\core;
 
-use yii\web\NotFoundHttpException;
 use yii2lab\core\domain\repositories\base\BaseActiveCoreRepository;
-use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
-use yii2module\account\domain\v2\entities\SecurityEntity;
 use yii2module\account\domain\v2\interfaces\repositories\SecurityInterface;
 
 class SecurityRepository extends BaseActiveCoreRepository implements SecurityInterface {
@@ -24,28 +21,6 @@ class SecurityRepository extends BaseActiveCoreRepository implements SecurityInt
 			'password' => $password,
 			'email' => $email,
 		]);
-	}
-	
-	/**
-	 * @param string $token
-	 * @param string $type
-	 *
-	 * @return SecurityEntity
-	 */
-	public function oneByToken($token, $type = null) {
-		// TODO: Implement oneByToken() method.
-	}
-	
-	/**
-	 * @param integer $userId
-	 * @param string  $password
-	 *
-	 * @return SecurityEntity|false
-	 * @throws UnprocessableEntityHttpException
-	 * @throws NotFoundHttpException
-	 */
-	public function validatePassword($userId, $password) {
-		// TODO: Implement validatePassword() method.
 	}
 	
 	/**
