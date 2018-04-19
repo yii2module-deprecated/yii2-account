0b4c0f043e53dc87125f4f33df76fd1787678d2b
Vitaliy <theyamshikov@yandex.ru>
2018-04-19 22:11:56 +0600

    fix

--- src/domain/v2/services/core/RegistrationService.php
+++ src/domain/v2/services/core/RegistrationService.php
@@ -2,12 +2,19 @@
 
 namespace yii2module\account\domain\v2\services\core;
 
+use yii2lab\core\domain\repositories\base\BaseCoreRepository;
 use yii2lab\domain\helpers\Helper;
 use yii2lab\domain\services\CoreBaseService;
-use common\enums\app\ApiVersionEnum;
 use yii2module\account\domain\v2\forms\RegistrationForm;
 use yii2module\account\domain\v2\interfaces\services\RegistrationInterface;
 
+/**
+ * Class RegistrationService
+ *
+ * @package yii2module\account\domain\v2\services\core
+ *
+ * @property-read BaseCoreRepository $repository
+ */
 class RegistrationService extends CoreBaseService implements RegistrationInterface {
 	
 	public $point = 'registration';
@@ -15,25 +22,25 @@ class RegistrationService extends CoreBaseService implements RegistrationInterfa
 	public function createTempAccount($login, $email = null) {
 		$body = compact('login', 'email');
 		Helper::validateForm(RegistrationForm::class, $body, RegistrationForm::SCENARIO_REQUEST);
-		$this->client->post('create-account', $body);
+		$this->repository->post('create-account', $body);
 	}
 	
 	public function checkActivationCode($login, $activation_code) {
 		$body = compact('login', 'activation_code');
 		Helper::validateForm(RegistrationForm::class, $body, RegistrationForm::SCENARIO_CHECK);
-		$this->client->post('activate-account', $body);
+		$this->repository->post('activate-account', $body);
 	}
 	
 	public function activateAccount($login, $activation_code) {
 		$body = compact('login', 'activation_code');
 		Helper::validateForm(RegistrationForm::class, $body, RegistrationForm::SCENARIO_CHECK);
-		$this->client->post('activate-account', $body);
+		$this->repository->post('activate-account', $body);
 	}
 	
 	public function createTpsAccount($login, $activation_code, $password, $email = null) {
 		$body = compact('login', 'activation_code', 'password');
 		Helper::validateForm(RegistrationForm::class, $body, RegistrationForm::SCENARIO_CONFIRM);
-		$this->client->post('set-password', $body);
+		$this->repository->post('set-password', $body);
 	}
 
 }
--- src/domain/v2/services/core/RestorePasswordService.php
+++ src/domain/v2/services/core/RestorePasswordService.php
@@ -2,11 +2,19 @@
 
 namespace yii2module\account\domain\v2\services\core;
 
+use yii2lab\core\domain\repositories\base\BaseCoreRepository;
 use yii2lab\domain\helpers\Helper;
 use yii2module\account\domain\v2\forms\RestorePasswordForm;
 use yii2lab\domain\services\CoreBaseService;
 use yii2module\account\domain\v2\interfaces\services\RestorePasswordInterface;
 
+/**
+ * Class RestorePasswordService
+ *
+ * @package yii2module\account\domain\v2\services\core
+ *
+ * @property-read BaseCoreRepository $repository
+ */
 class RestorePasswordService extends CoreBaseService implements RestorePasswordInterface {
 	
 	public $point = 'auth/restore-password';
@@ -15,19 +23,19 @@ class RestorePasswordService extends CoreBaseService implements RestorePasswordI
 	public function request($login, $mail = null) {
 		$body = compact(['login']);
 		Helper::validateForm(RestorePasswordForm::class, $body, RestorePasswordForm::SCENARIO_REQUEST);
-		$this->client->post('request', $body);
+		$this->repository->post('request', $body);
 	}
 	
 	public function checkActivationCode($login, $activation_code) {
 		$body = compact(['login', 'activation_code']);
 		Helper::validateForm(RestorePasswordForm::class, $body, RestorePasswordForm::SCENARIO_CHECK);
-		$this->client->post('check-code', $body);
+		$this->repository->post('check-code', $body);
 	}
 	
 	public function confirm($login, $activation_code, $password) {
 		$body = compact(['login', 'activation_code', 'password']);
 		Helper::validateForm(RestorePasswordForm::class, $body, RestorePasswordForm::SCENARIO_CONFIRM);
-		$this->client->post('confirm', $body);
+		$this->repository->post('confirm', $body);
 	}
 	
 }
