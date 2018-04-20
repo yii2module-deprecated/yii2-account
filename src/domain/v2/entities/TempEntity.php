ecf970b4d973329b1eeb2646164b08b28b329b23
Vitaliy <theyamshikov@yandex.ru>
2018-04-20 18:43:19 +0600

    clean

--- src/domain/v2/entities/TempEntity.php
+++ src/domain/v2/entities/TempEntity.php
@@ -5,6 +5,18 @@ namespace yii2module\account\domain\v2\entities;
 use yii2lab\domain\BaseEntity;
 use yii2lab\domain\values\TimeValue;
 
+/**
+ * Class TempEntity
+ *
+ * @package yii2module\account\domain\v2\entities
+ *
+ * @property $login
+ * @property $email
+ * @property $activation_code
+ * @property $ip
+ * @property $password
+ * @property $created_at
+ */
 class TempEntity extends BaseEntity {
 
 	protected $login;
