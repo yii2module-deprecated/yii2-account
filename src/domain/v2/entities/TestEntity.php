ff7953c7ba114944819183839f7677c53e29f41e
Vitaliy <theyamshikov@yandex.ru>
2018-06-30 15:59:35 +0600

    clean

--- src/domain/v2/entities/TestEntity.php
+++ src/domain/v2/entities/TestEntity.php
@@ -4,6 +4,18 @@ namespace yii2module\account\domain\v2\entities;
 
 use yii2lab\domain\BaseEntity;
 
+/**
+ * Class TestEntity
+ *
+ * @package yii2module\account\domain\v2\entities
+ *
+ * @property $id integer
+ * @property $login string
+ * @property $username string
+ * @property $email string
+ * @property $role string
+ * @property $status integer
+ */
 class TestEntity extends BaseEntity {
 
 	protected $id;
