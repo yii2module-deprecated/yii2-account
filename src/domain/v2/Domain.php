f7b5bf0faad8eb874aec421d2e9b4ab805d6504f
Vitaliy <theyamshikov@yandex.ru>
2018-07-15 21:00:45 +0600

    clean

--- src/domain/v2/Domain.php
+++ src/domain/v2/Domain.php
@@ -21,15 +21,6 @@ use yii2lab\misc\enums\TimeEnum;
  * @property-read \yii2module\account\domain\v2\interfaces\services\TestInterface $test
  * @property-read \yii2module\account\domain\v2\interfaces\services\RbacInterface $rbac
  * @property-read \yii2module\account\domain\v2\interfaces\services\ConfirmInterface $confirm
- * @property-read \yii2module\account\domain\v2\interfaces\services\AuthInterface $auth
- * @property-read \yii2module\account\domain\v2\interfaces\services\ConfirmInterface $confirm
- * @property-read \yii2module\account\domain\v2\interfaces\services\LoginInterface $login
- * @property-read \yii2module\account\domain\v2\interfaces\services\RbacInterface $rbac
- * @property-read \yii2module\account\domain\v2\interfaces\services\RegistrationInterface $registration
- * @property-read \yii2module\account\domain\v2\interfaces\services\RestorePasswordInterface $restorePassword
- * @property-read \yii2module\account\domain\v2\interfaces\services\SecurityInterface $security
- * @property-read \yii2module\account\domain\v2\interfaces\services\TempInterface $temp
- * @property-read \yii2module\account\domain\v2\interfaces\services\TestInterface $test
  * @property-read \yii2module\account\domain\v2\interfaces\repositories\RepositoriesInterface $repositories
  * @property-read \yii2module\account\domain\v2\interfaces\services\TokenInterface $token
  */
