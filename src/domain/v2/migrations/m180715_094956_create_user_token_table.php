457bcf6f32915d7c1f4576dbe2fa466df8a046de
Vitaliy <theyamshikov@yandex.ru>
2018-07-15 15:57:53 +0600

    create token migration

---
+++ src/domain/v2/migrations/m180715_094956_create_user_token_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii2lab\db\domain\db\MigrationCreateTable as Migration;
+
+/**
+ * Class m180715_094956_create_user_token_table
+ * 
+ * @package 
+ */
+class m180715_094956_create_user_token_table extends Migration {
+
+	public $table = '{{%user_token}}';
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getColumns()
+	{
+		return [
+			'user_id' => $this->integer(11)->notNull(),
+			'token' => $this->primaryKey(255)->notNull(),
+			'ip' => $this->string(40)->notNull(),
+			'platform' => $this->string(32)->notNull(),
+			'browser' => $this->string(32)->notNull(),
+			'version' => $this->string(10)->notNull(),
+			'created_at' => $this->string()->notNull(),
+		];
+	}
+
+	public function afterCreate()
+	{
+		$this->myAddForeignKey(
+			'user_id',
+			'{{%user}}',
+			'id',
+			'CASCADE',
+			'CASCADE'
+		);
+		$this->myCreateIndexUnique('token');
+	}
+
+}
