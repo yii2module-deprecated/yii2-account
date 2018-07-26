206e25c50dc3b815a341e42cfbddc4694b596308
Vitaliy <theyamshikov@yandex.ru>
2018-07-26 21:36:24 +0600

    make test sqlite

--- tests/_application/common/config/env-local.php
+++ tests/_application/common/config/env-local.php
@@ -4,7 +4,14 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-
+	'servers' => [
+		'db' => [
+			'test' => [
+				'driver' => null,
+				'dsn' => 'sqlite:@common/runtime/sqlite/test-package.db',
+			],
+		],
+	],
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/env-local.php');
