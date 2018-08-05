cc5039d24ecbd7bd3290b12098a97f376690c1f2
Vitaliy <theyamshikov@yandex.ru>
2018-08-05 22:37:10 +0600

    fix test

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -6,6 +6,7 @@ use yii2lab\domain\enums\Driver;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
+	'lang' => 'yii2module\lang\domain\Domain',
 	'rbac' => 'yii2lab\rbac\domain\Domain',
 	'account' => [
 		'class' => Domain::class,
