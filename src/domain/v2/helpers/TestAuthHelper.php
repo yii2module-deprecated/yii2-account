59de19c4a03ef72ae46a006779ac9ee72008e880
amubarak <amubarak@wooppay.com>
2018-09-17 15:35:23 +0600

    add auth pseudo test

--- src/domain/v2/helpers/TestAuthHelper.php
+++ src/domain/v2/helpers/TestAuthHelper.php
@@ -4,6 +4,7 @@ namespace yii2module\account\domain\v2\helpers;
 
 use Yii;
 use yii2lab\domain\helpers\DomainHelper;
+use yii2lab\helpers\ClientHelper;
 use yii2module\account\domain\v2\entities\LoginEntity;
 
 class TestAuthHelper {
@@ -24,6 +25,11 @@ class TestAuthHelper {
 		Yii::$app->user->setIdentity($userEntity);
 	}
 	
+	public static function authPseudo($login, $email = null, $parentLogin = null) {
+		self::defineAccountDomain();
+		$userEntity = Yii::$domain->account->authPseudo->authentication($login, $address = ClientHelper::ip(), $email = null, $parentLogin = null);
+		Yii::$app->user->setIdentity($userEntity);
+	}
 	public static function defineAccountDomain() {
 		DomainHelper::defineDomain('account', self::DOMAIN_CLASS);
 	}
