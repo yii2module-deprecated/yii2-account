ab5870e332a19895f794741735b568e27dae3fe4
amubarak <amubarak@wooppay.com>
2019-04-22 13:55:53 +0600

    g

--- src/api/v2/controllers/AuthController.php
+++ src/api/v2/controllers/AuthController.php
@@ -64,7 +64,7 @@ class AuthController extends Controller
 	
 	public function actionLogin()
 	{
-		prr(Yii::$app->request,1,1);
+		prr(Yii::$app->request->headers,1,1);
 		$body = Yii::$app->request->getBodyParams();
 		try {
 			$ip = ClientHelper::ip();
