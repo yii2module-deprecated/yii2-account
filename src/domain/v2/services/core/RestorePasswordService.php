dcd6fef071323b3621ca0844eaaadc312b0da58b
abykova <abykova@wooppay.com>
2019-07-31 15:50:18 +0600

    [fix] restore password

--- src/domain/v2/services/core/RestorePasswordService.php
+++ src/domain/v2/services/core/RestorePasswordService.php
@@ -22,8 +22,7 @@ class RestorePasswordService extends CoreBaseService implements RestorePasswordI
 	
 	public function request($login, $mail = null) {
 		Helper::validateForm(RestorePasswordForm::class, $login, RestorePasswordForm::SCENARIO_REQUEST);
-		$data = $this->repository->post('request', $login);
-		return $data->data;
+		$this->repository->post('request', $login);
 	}
 
 	public function checkActivationCode($login, $activation_code, $password) {
