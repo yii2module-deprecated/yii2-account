f5eb23937d16cb7b6ce6f28d64675f671a995303
dkushkin <dkushkin@wooppay.com>
2019-09-03 17:02:32 +0600

    Fixed bug with OTP and did nice error.

--- src/api/v2/controllers/AuthController.php
+++ src/api/v2/controllers/AuthController.php
@@ -4,12 +4,14 @@ namespace yii2module\account\api\v2\controllers;
 
 use Yii;
 use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
+use yii2lab\domain\helpers\ErrorCollection;
 use yii2lab\domain\helpers\Helper;
 use yii2lab\extension\web\helpers\Behavior;
 use yii2lab\extension\web\helpers\ClientHelper;
 use yii2lab\rest\domain\rest\Controller;
 use yii2woop\common\domain\account\v2\forms\AuthPseudoForm;
 use yii2module\account\domain\v2\interfaces\services\AuthInterface;
+use yii2woop\generated\exception\tps\SubjectOtpRequiredException;
 
 /**
  * Class AuthController
@@ -67,15 +69,20 @@ class AuthController extends Controller
 		Yii::$app->log->targets = [];
 		$body = Yii::$app->request->getBodyParams();
 		try {
-			$ip = ClientHelper::ip();
-			$entity = $this->service->authentication2($body, $ip);
-			Yii::$app->response->headers->set('Authorization', $entity->token);
-			return $entity;
-		} catch(UnprocessableEntityHttpException $e) {
+            $ip = ClientHelper::ip();
+            $entity = $this->service->authentication2($body, $ip);
+            Yii::$app->response->headers->set('Authorization', $entity->token);
+            return $entity;
+        } catch(UnprocessableEntityHttpException $e) {
 			Yii::$app->response->setStatusCode(422);
 			$response = $e->getErrors();
 			return $response;
-		}
+		} catch(SubjectOtpRequiredException $e) {
+            $error = new ErrorCollection;
+            $error->add('otp', 'account/main', 'empty.otp');
+            Yii::$app->response->setStatusCode(422);
+            return $error->all();
+        }
 	}
 	
 	public function actionPseudo()
