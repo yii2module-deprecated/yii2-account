7cdfacfe7ca44e1226ea1d244fc906d6681ec85c
amubarak@wooppay.kz <amubarak@wooppay.kz>
2019-11-11 16:15:17 +0600

    test fix

---
+++ src/domain/v2/fixtures/data/user_address.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	77771111111 => [
+		'login' => '77771111111',
+		'country_id' => null,
+		'region_id' => 4,
+		'city_id' => 59,
+		'district' => null,
+		'street' => 'rqwerqwr',
+		'home' => '5646',
+		'apartment' => 44,
+		'post_code' => null,
+	],
+];
---
+++ src/domain/v2/fixtures/data/user_registration.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	[
+		'login' => '77777777757',
+		'email' => 'Asdasd@sdf.ru',
+		'activation_code' => '818730',
+		'ip' => null,
+		'create_time' => null,
+		'created_at' => '2018-07-17 04:45:39',
+	],
+];
